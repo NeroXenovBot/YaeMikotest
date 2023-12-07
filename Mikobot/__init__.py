bbe89a66983d4e9cbd70f72425fca282da7d051c
NeroXenovBot <149504056+NeroXenovBot@users.noreply.github.com>
2023-12-07 15:00:17 +0530

    __init__.py

--- Mikobot/__init__.py
+++ Mikobot/__init__.py
@@ -182,20 +182,16 @@ function = dispatcher.add_handler
 
 # <================================================ BOOT MESSAGE=======================================================>
 ALIVE_MSG = """
-💫 *MY SYSTEM IS STARTING, PLEASE WAIT FOR SOMETIME TO COMPLETE BOOT!*
+😏 *MY SYSTEM IS STARTING, PLEASE WAIT FOR SOMETIME TO COMPLETE BOOT!*
 
 
 *IF COMMANDS DON'T WORK CHECK THE LOGS*
 """
 
 ALIVE_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "",
+    "https://telegra.ph/file/8440c976851a32e1e4bc0.jpg",
+    "https://telegra.ph/file/21ed517b92e677ce967a2.jpg",
 ]
 # <=======================================================================================================>
 
