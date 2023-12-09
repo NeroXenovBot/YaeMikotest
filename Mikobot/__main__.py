90414f89c22770c1a025176534ff99312befa9cf
NeroXenovBot <149504056+NeroXenovBot@users.noreply.github.com>
2023-12-09 06:33:27 +0530

    __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -249,7 +249,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(START_BTN),
-                parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=False,
             )
     else:
